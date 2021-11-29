ce62f8781d4adbf4ed3298b9e6287f7d0b2082f0
gir15 <gir15@LAPTOP-IP958APD>
2021-11-28 22:16:05 -0300

    Conexion BD - subida 1

---
+++ src/BD/Conexion.java
@@ -0,0 +1,29 @@
+
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author gir15
+ */
+public class Conexion {
+    
+    public Connection obtenerConexion(){
+        
+        Connection connection = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/entradacine","root","");
+            System.out.println("Conexión exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return connection;
+    }
+    
+}
